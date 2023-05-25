72c54eb8cea4ce656fc7c560dc2af1096ab4efe7
Tarasios <sircassiel@gmail.com>
2023-05-25 13:25:12 -0700

    Hotfix index.js
    Removed duplicate url declaration

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const navLinks = [
     {name: "Favourites", link: "/favourites"},
     {name: "Playlists", link: "/playlists"},
 ];
-const url = require("url");
+
 app.use("/", (req, res, next) => {
     app.locals.navLinks = navLinks;
     app.locals.currentUrl = url.parse(req.url).pathname;
